1ebd851a43331041df2ecceff562551daf6861fe
AF-1 <>
2021-03-03 14:26:02 +0100

    dstm changes

--- RatingsLight/Settings/DSTM.pm
+++ RatingsLight/Settings/DSTM.pm
@@ -21,7 +21,7 @@ my $plugin;
 sub new {
 	my $class = shift;
 	$plugin = shift;
-	$class->SUPER::new($plugin);
+	$class->SUPER::new($plugin,1);
 }
 
 sub name {
@@ -54,31 +54,19 @@ sub handler {
 	my $result = undef;
 	my $callHandler = 1;
 	if ($paramRef->{'saveSettings'}) {
- 		my $excludegenres;
- 		my $excludegenres_namelist;
-		my $dbh = Slim::Schema->storage->dbh();
-		my $sqlstatement = "select id,name from genres";
-		my $sth = $dbh->prepare( $sqlstatement );
-		$sth->execute();
-		my ($excludedgenre_id, $excludedgenre_name);
-		$sth->bind_col(1,\$excludedgenre_id);
-		$sth->bind_col(2,\$excludedgenre_name);
 
-		while( $sth->fetch()) {
- 			my $excludedgenre_value = $paramRef->{"pref_excludedgenre_$excludedgenre_id"};
- 			if ((defined $excludedgenre_value) && ($excludedgenre_value == 1)) {
-				push (@$excludegenres, {id => $excludedgenre_id, name => $excludedgenre_name, chosen => 'yes'});
-  				push(@$excludegenres_namelist, $excludedgenre_name);
- 			} else {
- 				push (@$excludegenres, {id => $excludedgenre_id, name => $excludedgenre_name, chosen => ''});
- 			}
+		my $excludegenres_namelist;
+
+		my $genres = getGenres($client);
+
+		# %$paramRef will contain a key called genre_<genre id> for each ticked checkbox on the page
+		for my $genre (keys %{$genres}) {
+			if ($paramRef->{'genre_'.$genres->{$genre}->{'id'}}) {
+				push (@$excludegenres_namelist, $genre);
+			}
 		}
-		$sth->finish();
-
- 		$prefs->set('excludegenres_namelist', $excludegenres_namelist);
- 		$log->debug('*** saved *** excludegenres_namelist = '.Dumper($excludegenres_namelist));
- 		$paramRef->{@$excludegenres} = $excludegenres;
- 		#$log->debug('*** saved *** excludegenres = '.Dumper(@$excludegenres));
+		$log->debug("*** SAVED *** excludegenres_namelist = ".Dumper($excludegenres_namelist));
+		$prefs->set('excludegenres_namelist', $excludegenres_namelist);
 
 		$result = $class->SUPER::handler($client, $paramRef);
 		$callHandler = 0;
@@ -87,48 +75,46 @@ sub handler {
 	}
 
 	# push to settings page
+	my $genrelist = getGenres();
+	$log->debug("genrelist (all genres) = ".Dumper($genrelist));
+	$paramRef->{'genrelist'} = $genrelist;
 
-	$paramRef->{excludedgenrelist} = [];
-
-	my $excludegenres;
-	my $excludegenres_namelist = $prefs->get('excludegenres_namelist');
-	$log->debug('*** loaded *** excludegenres_namelist = '.Dumper($excludegenres_namelist));
-
-	my $dbh = Slim::Schema->storage->dbh();
-	my $sqlstatement = "select id,name from genres order by namesort asc";
-
-	my $sth = $dbh->prepare( $sqlstatement );
-	$sth->execute();
-
-	my ($excludedgenre_id, $excludedgenre_name);
-	$sth->bind_col(1,\$excludedgenre_id);
-	$sth->bind_col(2,\$excludedgenre_name);
-
-	while( $sth->fetch()) {
-		my %excludedgenres_chosen = map { $_ => 1 } @$excludegenres_namelist;
-		if(exists($excludedgenres_chosen{$excludedgenre_name})) {
-			push (@$excludegenres, {id => $excludedgenre_id, name => smartdecode($excludedgenre_name), chosen => 'yes'});
-		} else {
-			push (@$excludegenres, {id => $excludedgenre_id, name => smartdecode($excludedgenre_name), chosen => ''});
-		}
-	}
-	$sth->finish();
-
-	foreach my $thisgenre (@$excludegenres) {
-			push( @{$paramRef->{excludedgenrelist}}, $thisgenre);
-	}
+	my $genrelistsorted = [getSortedGenres()];
+	$log->debug("genrelistsorted (just names) = ".Dumper($genrelistsorted));
+	$paramRef->{'genrelistsorted'} = $genrelistsorted;
 
 	$result = $class->SUPER::handler($client, $paramRef);
 
 	return $result;
 }
 
-sub smartdecode {
-    use URI::Escape qw( uri_unescape );
-    use utf8;
-    my $x = my $y = uri_unescape($_[0]);
-    return $x if utf8::decode($x);
-    return $y;
+sub getGenres {
+	my $genres = {};
+	my $query = ['genres', 0, 999_999];
+
+	my $request = Slim::Control::Request::executeRequest(undef, $query);
+
+	# Extract each genre name into a hash
+	my %exclude = map { $_ => 1 } @{ $prefs->get('excludegenres_namelist') };
+
+	my $i = 0;
+	foreach my $genre ( @{ $request->getResult('genres_loop') || [] } ) {
+		my $name = $genre->{genre};
+		$genres->{$name} = {
+			'name' => $name,
+			'id' => $genre->{id},
+			'chosen' => $exclude{$name} ? 'yes' : '',
+			'sort' => $i++,
+		};
+	}
+	return $genres;
+}
+
+sub getSortedGenres {
+	my $genres = getGenres();
+	return sort {
+		$genres->{$a}->{sort} <=> $genres->{$b}->{sort};
+	} keys %$genres;
 }
 
 1;
